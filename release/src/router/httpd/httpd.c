3d7d9189e5edc9b6b68049973403f84e757b8586
Eric Sauvageau <rmerl@lostrealm.ca>
2012-06-24 02:53:36 -0400

    Fixed https auth being skipped (bug in Asus's code)

--- release/src/router/httpd/httpd.c
+++ release/src/router/httpd/httpd.c
@@ -957,6 +957,10 @@ void http_logout(unsigned int ip) {
 int is_auth(void)
 {
 	if (http_port==SERVER_PORT ||
+#ifdef RTCONFIG_HTTPS
+                http_port==SERVER_PORT_SSL ||
+                http_port==nvram_get_int("https_lanport") ||
+#endif
 		strcmp(nvram_get_x("PrinterStatus", "usb_webhttpcheck_x"), "1")==0) return 1;
 	else return 0;
 }
