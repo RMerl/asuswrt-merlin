27c939e290d757ebaf1b4c6c5611e47a7f3aa528
Eric Sauvageau <rmerl@lostrealm.ca>
2012-08-08 17:07:18 -0400

    Removed development code

--- release/src-rt/linux/linux-2.6/drivers/mtd/47xxnand/brcmnand_47xx.c
+++ release/src-rt/linux/linux-2.6/drivers/mtd/47xxnand/brcmnand_47xx.c
@@ -1834,22 +1834,6 @@ struct mtd_partition *init_brcmnand_mtd_partitions(struct mtd_info *mtd, size_t 
 		j++;
 #endif
 	}
-/*
-	if (size > ((NFL_BBT_SIZE * 2) + (mtd->erasesize * PART_JFFS2_MIN)) ) {
-		brcmnand_parts[j].name = "jffs2";
-		brcmnand_parts[j].offset = offset;
-
-		if ( (size - (NFL_BBT_SIZE * 2)) > (32 * 0x100000UL) ) // Limit to 32 MB max
-			jffssize = (32 * 0x100000UL);
-		else
-			jffssize = size - (NFL_BBT_SIZE * 2);
-
-		brcmnand_parts[j].size = jffssize;
-		offset += brcmnand_parts[j].size;
-		size -= brcmnand_parts[j].size;
-		j++;
-	}
-*/
 
 	jffssize = size - (NFL_BBT_SIZE * 2);
 	if (jffssize > (mtd->erasesize * PART_JFFS2_MIN)) {
