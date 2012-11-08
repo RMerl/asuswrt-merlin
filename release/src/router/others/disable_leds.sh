726f7ef81639e0cb0ac8cc25c313a0525a275310
Eric Sauvageau <rmerl@lostrealm.ca>
2012-11-07 22:00:57 -0500

    Removed, since they are now implemented in rc

--- release/src/router/others/disable_leds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-nvram set led_disable=1
-nvram commit
-wl leddc 1
-led_ctrl 0 0
-led_ctrl 1 0
-led_ctrl 20 0
--- release/src/router/others/restore_leds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-nvram set led_disable=0
-nvram commit
-wl leddc 0
-led_ctrl 0 1
-led_ctrl 1 1
-led_ctrl 20 1
