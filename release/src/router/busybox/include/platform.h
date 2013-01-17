068ba82d1e7972c4c22ae507669ce958b48de3b5
Eric Sauvageau <rmerl@lostrealm.ca>
2013-01-17 01:29:01 -0500

    140-ls-sort-version patch

--- release/src/router/busybox/include/platform.h
+++ release/src/router/busybox/include/platform.h
@@ -366,7 +366,7 @@ typedef unsigned smalluint;
 #if defined(__UCLIBC_MAJOR__)
 # if __UCLIBC_MAJOR__ == 0 \
   && (   __UCLIBC_MINOR__ < 9 \
-     || (__UCLIBC_MINOR__ == 9 && __UCLIBC_SUBLEVEL__ < 32) \
+     || (__UCLIBC_MINOR__ == 9 && __UCLIBC_SUBLEVEL__ < 30) \
      )
 #  undef HAVE_STRVERSCMP
 # endif
