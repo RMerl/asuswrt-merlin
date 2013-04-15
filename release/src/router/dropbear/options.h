f8ab65ef7ae323060dc09000f3506ddd57e0b51a
dodava <sedlmeyer@gmail.com>
2013-04-15 14:48:14 -0300

    Change dropbear SFTPSERVER_PATH
    Change dropbear SFTPSERVER_PATH to /opt/libexec/sftp-server. This allows enabling sftp support by installing either the entware or optware openssh-sftp-server package.

--- release/src/router/dropbear/options.h
+++ release/src/router/dropbear/options.h
@@ -250,7 +250,7 @@ much traffic. */
  * OpenSSH), set the path below. If the path isn't defined, sftp will not
  * be enabled */
 #ifndef SFTPSERVER_PATH
-#define SFTPSERVER_PATH "/usr/libexec/sftp-server"
+#define SFTPSERVER_PATH "/opt/libexec/sftp-server"
 #endif
 
 /* This is used by the scp binary when used as a client binary. If you're
