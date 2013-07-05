c0b002276fa89bbe8a525529c8ddfdd4ba4f2982
Eric Sauvageau <rmerl@lostrealm.ca>
2013-07-05 01:10:07 -0400

    Commit back cleared flag to nvram

--- release/src/router/rc/jffs2.c
+++ release/src/router/rc/jffs2.c
@@ -56,8 +56,9 @@ _dprintf("*** jffs2: %d, %d\n", part, size);
 		nvram_set("jffs2_format", "0");
 
 		if( (model==MODEL_RTAC56U || model==MODEL_RTAC68U) ^ (!mtd_erase(JFFS_NAME)) ){
-                        error("formatting");
-                        return;
+			error("formatting");
+			nvram_commit_x();
+			return;
 		}
 
 		format = 1;
