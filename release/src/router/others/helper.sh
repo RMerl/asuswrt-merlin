88cd978a72b83b4dd342b0fb18c2d9e63ab8b56b
Eric Sauvageau <rmerl@lostrealm.ca>
2014-01-25 23:29:14 -0500

    Added helper.sh script to make it easier to use Postconf scripts
    By sourcing /usr/sbin/helper.sh, postconf script users can easily
    do replace/append operations on config scripts.  Example:
    
    source /usr/sbin/helper.sh
    pc_replace "sendfile = no" "sendfile = yes" /etc/smb.conf

---
+++ release/src/router/others/helper.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Asuswrt-Merlin helper functions
+# For use with Postconf scripts (and others)
+
+_quote () {
+    echo $1 | sed 's/[]\.|$(){}?+*^[]/\\&/g'
+}
+
+# This function looks for a string, and inserts a specified string after it inside a given file
+# $1: the line to locate, $2: the line to insert, $3: Config file where to insert
+pc_insert () {
+	PATTERN=$(_quote "$1")
+	CONTENT=$(_quote "$2")
+	sed -i "/$PATTERN/a$CONTENT" $3
+}
+
+
+
+# This function looks for a string, and replace it with a different string inside a given file
+# $1: the line to locate, $2: the line to replace with, $3: Config file where to insert
+pc_replace() {
+        PATTERN=$(_quote "$1")
+        CONTENT=$(_quote "$2")
+	sed -i "s/$PATTERN/$CONTENT/" $3
+}
+
