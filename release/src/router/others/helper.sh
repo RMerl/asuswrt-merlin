aadaf0a8afd9228e845099cc0bdab79dfe0c8ef8
Eric Sauvageau <rmerl@lostrealm.ca>
2014-01-25 23:42:12 -0500

    Added pc_append(); cleaned up formatting

--- release/src/router/others/helper.sh
+++ release/src/router/others/helper.sh
@@ -3,13 +3,13 @@
 # Asuswrt-Merlin helper functions
 # For use with Postconf scripts (and others)
 
-_quote () {
+_quote() {
     echo $1 | sed 's/[]\.|$(){}?+*^[]/\\&/g'
 }
 
 # This function looks for a string, and inserts a specified string after it inside a given file
 # $1: the line to locate, $2: the line to insert, $3: Config file where to insert
-pc_insert () {
+pc_insert() {
 	PATTERN=$(_quote "$1")
 	CONTENT=$(_quote "$2")
 	sed -i "/$PATTERN/a$CONTENT" $3
@@ -20,8 +20,14 @@ pc_insert () {
 # This function looks for a string, and replace it with a different string inside a given file
 # $1: the line to locate, $2: the line to replace with, $3: Config file where to insert
 pc_replace() {
-        PATTERN=$(_quote "$1")
-        CONTENT=$(_quote "$2")
+	PATTERN=$(_quote "$1")
+	CONTENT=$(_quote "$2")
 	sed -i "s/$PATTERN/$CONTENT/" $3
 }
 
+# This function will append a given string at the end of a given file
+# $1 The line to append at the end, $2: Config file where to append
+pc_append() {
+	CONTENT=$(_quote "$1")
+	echo "$CONTENT" >> $2
+}
