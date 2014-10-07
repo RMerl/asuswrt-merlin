c5970d458436a230f06657973314dafd408dd5db
joejoejo <goodpets@gmail.com>
2014-10-06 20:18:13 -0400

    Update bcmenetmib.h

--- release/src-rt-6.x/et/include/bcmenetmib.h
+++ release/src-rt-6.x/et/include/bcmenetmib.h
@@ -3,7 +3,7 @@
  * Broadcom Home Networking Division
  * BCM44XX and BCM47XX 10/100 Mbps Ethernet cores.
  *
- * Copyright (C) 2012, Broadcom Corporation. All Rights Reserved.
+ * Copyright (C) 2013, Broadcom Corporation. All Rights Reserved.
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -16,7 +16,7 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * $Id: bcmenetmib.h 376345 2012-12-24 21:13:06Z $
+ * $Id: bcmenetmib.h 376344 2012-12-24 21:10:09Z $
  */
 
 #ifndef _bcmenetmib_h_
