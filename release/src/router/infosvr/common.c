c9ea1bc339024bb09b5cd8e45ae1a9922e73884f
theAprel <theAprel@users.noreply.github.com>
2015-01-09 00:48:10 -0500

    Revert "hack out CVE-2014-9583"
    This reverts commit 74b90d4790d1eeaf3d725f4b9305b3d09adafec1.

--- release/src/router/infosvr/common.c
+++ release/src/router/infosvr/common.c
@@ -176,5 +176,441 @@ int is_psta(int unit)
 
 char *processPacket(int sockfd, char *pdubuf)
 {
+    IBOX_COMM_PKT_HDR	*phdr;
+    IBOX_COMM_PKT_HDR_EX *phdr_ex;
+    IBOX_COMM_PKT_RES_EX *phdr_res;
+    PKT_GET_INFO *ginfo;
+
+#ifdef WAVESERVER	// eric++
+    int fail = 0;
+    pid_t pid;
+    DIR *dir;
+    int fd, ret, bytes;
+    unsigned char tmp_buf[15];	// /proc/XXXXXX
+    WS_INFO_T *wsinfo;
+#endif
+//#ifdef WL700G
+    STORAGE_INFO_T *st;
+//#endif
+//    int i;
+    char ftype[8], prinfo[128];	/* get disk type */
+    int free_space;
+#if defined(RTCONFIG_WIRELESSREPEATER) || defined(RTCONFIG_PROXYSTA)
+    char tmp[100], prefix[] = "wlXXXXXXXXXXXXXX";
+#endif
+
+    phdr = (IBOX_COMM_PKT_HDR *)pdubuf;  
+    phdr_res = (IBOX_COMM_PKT_RES_EX *)pdubuf_res;
+    
+//    fprintf(stderr,"Get: %x %x %x\n", phdr->ServiceID, phdr->PacketType, phdr->OpCode);
+
+    if (phdr->ServiceID==NET_SERVICE_ID_IBOX_INFO &&
+	phdr->PacketType==NET_PACKET_TYPE_CMD)
+    {	    
+	if (phdr->OpCode!=NET_CMD_ID_GETINFO && phdr->OpCode!=NET_CMD_ID_GETINFO_MANU&&
+	    phdr_res->OpCode==phdr->OpCode &&
+	    phdr_res->Info==phdr->Info)
+	{	
+		// if transaction id is equal to the transaction id of the last response message, just re-send message again;
+		return pdubuf_res;
+	}	
+	
+	phdr_res->ServiceID=NET_SERVICE_ID_IBOX_INFO;
+	phdr_res->PacketType=NET_PACKET_TYPE_RES;
+	phdr_res->OpCode=phdr->OpCode;
+
+	if (phdr->OpCode!=NET_CMD_ID_GETINFO && phdr->OpCode!=NET_CMD_ID_GETINFO_MANU)
+	{
+		phdr_ex = (IBOX_COMM_PKT_HDR_EX *)pdubuf;	
+		
+		// Check Mac Address
+		if (memcpy(phdr_ex->MacAddress, mac, 6)==0)
+		{
+			_dprintf("Mac Error %2x%2x%2x%2x%2x%2x\n",
+				(unsigned char)phdr_ex->MacAddress[0],
+				(unsigned char)phdr_ex->MacAddress[1],
+				(unsigned char)phdr_ex->MacAddress[2],
+				(unsigned char)phdr_ex->MacAddress[3],
+				(unsigned char)phdr_ex->MacAddress[4],
+				(unsigned char)phdr_ex->MacAddress[5]
+				);
+			return NULL;
+		}
+		
+		// Check Password
+		//if (strcmp(phdr_ex->Password, "admin")!=0)
+		//{
+		//	phdr_res->OpCode = phdr->OpCode | NET_RES_ERR_PASSWORD;
+		//	_dprintf("Password Error %s\n", phdr_ex->Password);	
+		//	return NULL;
+		//}
+		phdr_res->Info = phdr_ex->Info;
+		memcpy(phdr_res->MacAddress, phdr_ex->MacAddress, 6);
+	}
+
+	switch(phdr->OpCode)
+	{
+		case NET_CMD_ID_GETINFO_MANU:
+//		case NET_CMD_ID_GETINFO:
+		     _dprintf("NET CMD GETINFO_MANU\n");	// tmp test
+		     ginfo=(PKT_GET_INFO *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES));
+		     memset(ginfo, 0, sizeof(ginfo));
+#if 0
+#ifdef PRNINFO
+		     readPrnID(ginfo->PrinterInfo);
+#else
+		     memset(ginfo->PrinterInfo, 0, sizeof(ginfo->PrinterInfo));
+#endif
+#else
+			if (strlen(nvram_safe_get("u2ec_mfg")) && strlen(nvram_safe_get("u2ec_device")))
+			{
+				if (strstr(nvram_safe_get("u2ec_device"), nvram_safe_get("u2ec_mfg")))
+					sprintf(ginfo->PrinterInfo, "%s", nvram_safe_get("u2ec_device"));
+				else
+					sprintf(ginfo->PrinterInfo, "%s %s", nvram_safe_get("u2ec_mfg"), nvram_safe_get("u2ec_device"));
+			}
+#endif
+#ifdef RTCONFIG_WIRELESSREPEATER
+			if (nvram_get_int("sw_mode") == SW_MODE_REPEATER)
+			{
+				snprintf(prefix, sizeof(prefix), "wl%d.1_", nvram_get_int("wlc_band"));
+				strncpy(ssid_g, nvram_safe_get(strcat_r(prefix, "ssid", tmp)), 32);
+			}
+			else
+#endif
+#ifdef RTCONFIG_BCMWL6
+#ifdef RTCONFIG_PROXYSTA
+			if (is_psta(0) || is_psta(1))
+			{
+				snprintf(prefix, sizeof(prefix), "wl%d_", 1 - nvram_get_int("wlc_band"));
+				strncpy(ssid_g, nvram_safe_get(strcat_r(prefix, "ssid", tmp)), 32);
+			}
+			else
+#endif
+#endif
+		     strncpy(ssid_g, nvram_safe_get("wl0_ssid"), 32);
+		     strcpy(ginfo->SSID, ssid_g);
+		     strcpy(ginfo->NetMask, get_lan_netmask());
+		     strcpy(ginfo->ProductID, productid_g);	// disable for tmp
+		     strcpy(ginfo->FirmwareVersion, firmver_g);	// disable for tmp
+		     memcpy(ginfo->MacAddress, mac, 6);
+#ifdef WCLIENT
+		     ginfo->OperationMode = OPERATION_MODE_WB;
+		     ginfo->Regulation = 0xff;
+#endif
+	#ifdef WAVESERVER
+	     st = (STORAGE_INFO_T *) (pdubuf_res + sizeof (IBOX_COMM_PKT_RES) + sizeof (PKT_GET_INFO) + sizeof(WS_INFO_T));
+	#else
+	     st = (STORAGE_INFO_T *) (pdubuf_res + sizeof (IBOX_COMM_PKT_RES) + sizeof (PKT_GET_INFO));
+	#endif
+	//printf("get storage status(1)\n");	// tmp test
+		     getStorageStatus(st);
+//#endif /* #ifdef WL700G */
+ 		     /* Disable  WSC functions for MFG test*/
+		     nvram_set("asus_mfg", "1");
+/*
+		     nvram_set("wsc_config_state", "1");
+		     system("killall wsccmd");
+		     system("killall wsc");
+		     system("killall upnp");
+		     system("killall ntp");
+	 	     system("killall ntpclient");
+		     system("killall lld2d");
+*/
+		     sendInfo(sockfd, pdubuf_res);
+		     return pdubuf_res;
+
+		case NET_CMD_ID_GETINFO:
+			_dprintf("NET CMD GETINFO\n");
+		     ginfo=(PKT_GET_INFO *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES));
+		     memset(ginfo, 0, sizeof(ginfo));
+#if 0
+#ifdef PRNINFO
+    		     readPrnID(ginfo->PrinterInfo);
+#else
+		     memset(ginfo->PrinterInfo, 0, sizeof(ginfo->PrinterInfo));
+#endif
+#else
+			if (strlen(nvram_safe_get("u2ec_mfg")) && strlen(nvram_safe_get("u2ec_device")))
+			{
+				if (strstr(nvram_safe_get("u2ec_device"), nvram_safe_get("u2ec_mfg")))
+					sprintf(ginfo->PrinterInfo, "%s", nvram_safe_get("u2ec_device"));
+				else
+					sprintf(ginfo->PrinterInfo, "%s %s", nvram_safe_get("u2ec_mfg"), nvram_safe_get("u2ec_device"));
+			}
+#endif
+#ifdef RTCONFIG_WIRELESSREPEATER
+			if (nvram_get_int("sw_mode") == SW_MODE_REPEATER)
+			{
+				snprintf(prefix, sizeof(prefix), "wl%d.1_", nvram_get_int("wlc_band"));
+				strncpy(ssid_g, nvram_safe_get(strcat_r(prefix, "ssid", tmp)), 32);
+			}
+			else
+#endif
+#ifdef RTCONFIG_BCMWL6
+#ifdef RTCONFIG_PROXYSTA
+			if (is_psta(0) || is_psta(1))
+			{
+				snprintf(prefix, sizeof(prefix), "wl%d_", 1 - nvram_get_int("wlc_band"));
+				strncpy(ssid_g, nvram_safe_get(strcat_r(prefix, "ssid", tmp)), 32);
+			}
+			else
+#endif
+#endif
+		     strncpy(ssid_g, nvram_safe_get("wl0_ssid"), 32);
+   		     strcpy(ginfo->SSID, ssid_g);
+		     strcpy(ginfo->NetMask, get_lan_netmask());
+		     strcpy(ginfo->ProductID, productid_g);	// disable for tmp
+		     strcpy(ginfo->FirmwareVersion, firmver_g); // disable for tmp
+		     memcpy(ginfo->MacAddress, mac, 6);
+
+#ifdef WAVESERVER    // eric++
+	     	     // search /tmp/waveserver and get information
+	     	     wsinfo = (WS_INFO_T*) (pdubuf_res + sizeof (IBOX_COMM_PKT_RES) + sizeof (PKT_GET_INFO));
+
+	     	     fd = open (WS_INFO_FILENAME, O_RDONLY);
+	     	     if (fd != -1)	{
+				bytes = sizeof (WS_INFO_T);
+				while (bytes > 0)	{
+			    	ret = read (fd, wsinfo, bytes); 
+		    		if (ret > 0)			{ bytes -= ret;		} 
+					else if (ret < 0)		{ fail++; break;	} 
+					else if (ret == 0)		{ fail++; break;	}
+		 		} /* while () */
+			} else {
+				fail++;
+			}
+
+			if (fail == 0 && bytes == 0)	{
+				ret = read (fd, &pid, sizeof (pid_t));
+				if (ret == sizeof (pid_t))	{
+			    	sprintf (tmp_buf, "/proc/%d", pid);
+			    	dir = opendir (tmp_buf);
+			    	if (dir == NULL)	{	// file exist, but the process had been killed
+		    		    fail++;
+				    }
+				    closedir (dir);
+				} else {	// file not found or error occurred
+					fail++;
+				}
+	   		}
+
+			if (fail != 0)	{
+		    		memset (wsinfo, 0, sizeof (WS_INFO_T));
+			}
+#endif /* #ifdef WAVESERVER */
+	#ifdef WAVESERVER
+	     		st = (STORAGE_INFO_T *) (pdubuf_res + sizeof (IBOX_COMM_PKT_RES) + sizeof (PKT_GET_INFO) + sizeof(WS_INFO_T));
+	#else
+	     		st = (STORAGE_INFO_T *) (pdubuf_res + sizeof (IBOX_COMM_PKT_RES) + sizeof (PKT_GET_INFO));
+	#endif
+		  	getStorageStatus(st);
+			sendInfo(sockfd, pdubuf_res);
+			return pdubuf_res;		     	
+
+		case NET_CMD_ID_GETINFO_EX2:
+		     _dprintf("\n we got case GETINFO_EX2\n");	// tmp test
+		     ginfo=(PKT_GET_INFO *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES));
+		     memset(ginfo, 0, sizeof(ginfo));
+		     memset(ginfo->PrinterInfo, 0, sizeof(ginfo->PrinterInfo));
+
+		     /* get disk type */
+		     memset(ftype, 0, sizeof(ftype));
+		     memset(prinfo, 0, sizeof(prinfo));
+		     free_space = get_ftype(ftype);
+		     _dprintf("get ftpye is %s, free = %d\n", ftype, free_space);
+		     sprintf(prinfo, "%s:%d!$", ftype, free_space);
+		     memcpy(ginfo->PrinterInfo, prinfo, strlen(prinfo));
+
+		     sendInfo(sockfd, pdubuf_res);
+		     return pdubuf_res;
+
+		case NET_CMD_ID_MANU_CMD:
+		{
+		     #define MAXSYSCMD 256
+		     char cmdstr[MAXSYSCMD];
+		     PKT_SYSCMD *syscmd;
+		     PKT_SYSCMD_RES *syscmd_res;
+		     FILE *fp;
+
+printf("1. NET_CMD_ID_MANU_CMD:\n");
+_dprintf("2. NET_CMD_ID_MANU_CMD:\n");
+fprintf(stderr, "3. NET_CMD_ID_MANU_CMD:\n");
+
+		     syscmd = (PKT_SYSCMD *)(pdubuf+sizeof(IBOX_COMM_PKT_HDR_EX));
+		     syscmd_res = (PKT_SYSCMD_RES *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES_EX));
+
+		     if (syscmd->len>=MAXSYSCMD) syscmd->len=MAXSYSCMD;
+		     syscmd->cmd[syscmd->len]=0;
+		     syscmd->len=strlen(syscmd->cmd);
+		     fprintf(stderr,"system cmd: %d %s\n", syscmd->len, syscmd->cmd);
+#if 0
+		     if (!strcmp(syscmd->cmd, "GetAliveUsbDeviceInfo"))
+		     {
+		     	// response format: "USB device name","current status of the device","IP of cccupied user"
+			if (nvram_match("u2ec_device", ""))
+				sprintf(syscmd_res->res, "\"%s\",\"%s\"", "", "");
+			else if (nvram_invmatch("u2ec_busyip", ""))
+//				sprintf(syscmd_res->res, "\"%s\",\"%s\",\"%s\"", nvram_safe_get("u2ec_device"), "Busy", nvram_safe_get("u2ec_busyip"));
+				sprintf(syscmd_res->res, "\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"", ssid_g, productid_g, nvram_safe_get("u2ec_device"), "Busy", nvram_safe_get("u2ec_busyip"));
+			else
+//				sprintf(syscmd_res->res, "\"%s\",\"%s\"", nvram_safe_get("u2ec_device"), "Idle");
+				sprintf(syscmd_res->res, "\"%s\",\"%s\",\"%s\",\"%s\"", ssid_g, productid_g, nvram_safe_get("u2ec_device"), "Idle");
+			syscmd_res->len = strlen(syscmd_res->res);
+			_dprintf("%d %s\n", syscmd_res->len, syscmd_res->res);
+//			kill_pidfile_s("/var/run/usdsvr_broadcast.pid", SIGUSR1);
+			sendInfo(sockfd, pdubuf_res);
+		     }
+		     else if (!strncmp(syscmd->cmd, "ClientPostMsg ", 14))
+		     {
+			char userip[16];
+		     	char usermsg[256];
+		     	char *p;
+		     	int flag_invalid = 0;
+
+			/* format: ClientPostMsg"Occupied User IP","ClientMsg" */
+			memset(userip, 0x0, 16);
+			memset(usermsg, 0x0, 256);
+			strncpy(userip, syscmd->cmd + 15, 16);	// skip "ClientPostMsg \""
+
+			if (p = strchr(userip, '"'))
+				*p = '\0';
+			else			// invalid format
+			{
+				flag_invalid = 1;
+				strcpy(userip, "255.255.255.255");
+			}
+
+			if (!flag_invalid)	// skip "ClientPostMsg\"Occupied User IP\",\""
+			{
+				strcpy(usermsg, syscmd->cmd + 15 + strlen(userip) + 3);
+				if (strlen(usermsg) > 1)
+					usermsg[strlen(usermsg) - 1] = '\0';
+			}
+			else
+				strcpy(usermsg, "invalid message format!");
+
+			if (!strcmp(userip, "255.255.255.255"))
+			{
+				nvram_set("u2ec_msg_broadcast", usermsg);
+				kill_pidfile_s("/var/run/usdsvr_broadcast.pid", SIGUSR2);
+			}
+			else
+			{
+				nvram_set("u2ec_clntip_unicast", userip);
+				nvram_set("u2ec_msg_unicast", usermsg);
+				kill_pidfile_s("/var/run/usdsvr_unicast.pid", SIGTSTP);
+			}
+
+//			strcpy(syscmd_res->res, usermsg);
+//		     	syscmd_res->len = strlen(syscmd_res->res);
+//		     	_dprintf("client ip: %s\n", userip);
+//			_dprintf("%d %s\n", syscmd_res->len, syscmd_res->res);
+//			sendInfo(sockfd, pdubuf_res);
+		     }
+		     else
+#endif
+		     {
+			sprintf(cmdstr, "%s > /tmp/syscmd.out", syscmd->cmd);
+			system(cmdstr);
+
+			fprintf(stderr,"rund: %s\n", cmdstr);
+			fp = fopen("/tmp/syscmd.out", "r");
+
+			if (fp!=NULL)
+			{
+				syscmd_res->len = fread(syscmd_res->res, 1, sizeof(syscmd_res->res), fp);
+				fclose(fp);
+			}
+			else syscmd_res->len=0;
+
+			fprintf(stderr,"%d %s\n", syscmd_res->len, syscmd_res->res);
+			/* repeat 3 times for MFG by Yen*/
+			sendInfo(sockfd, pdubuf_res);
+			sendInfo(sockfd, pdubuf_res);
+			sendInfo(sockfd, pdubuf_res);
+			if(strstr(syscmd->cmd, "Commit")) {
+				int x=0;
+				while(x<7) {
+					sendInfo(sockfd, pdubuf_res);
+					x++;
+				}
+			}
+			/* end of MFG */
+			fprintf(stderr,"\nSendInfo 3 times!\n");
+		     }
+	 	     return pdubuf_res;
+		}
+#ifdef BTN_SETUP // This option can not co-exist with WCLIENT
+		case NET_CMD_ID_SETKEY_EX:
+		{
+		     #define MAXSYSCMD 256
+		     char cmdstr[MAXSYSCMD];
+		     PKT_SET_INFO_GW_QUICK_KEY *pkey;
+		     PKT_SET_INFO_GW_QUICK_KEY *pkey_res;
+
+		     if (!is_setup_mode(BTNSETUP_START)) return NULL;
+		     pkey=(PKT_SET_INFO_GW_QUICK_KEY *)(pdubuf+sizeof(IBOX_COMM_PKT_HDR_EX));
+		     pkey_res = (PKT_SET_INFO_GW_QUICK_KEY *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES_EX));
+
+#ifdef ENCRYPTION
+		     if (pkey->KeyLen==0) return NULL;
+
+#else
+		     pkey_res->KeyLen=0;
+#endif
+		     sprintf(cmdstr, "%2x%2x%2x%2x%2x%2x\n",
+				(unsigned char)phdr_ex->MacAddress[0],
+				(unsigned char)phdr_ex->MacAddress[1],
+				(unsigned char)phdr_ex->MacAddress[2],
+				(unsigned char)phdr_ex->MacAddress[3],
+				(unsigned char)phdr_ex->MacAddress[4],
+				(unsigned char)phdr_ex->MacAddress[5]
+				);
+		     nvram_set("bs_mac", cmdstr);
+		     sprintf(cmdstr, "Set MAC %s", cmdstr);
+		     syslog(LOG_NOTICE, cmdstr);
+		     sendInfo(sockfd, pdubuf_res);
+		     return pdubuf_res;
+		}
+		case NET_CMD_ID_QUICKGW_EX:
+		{
+		     #define MAXSYSCMD 64
+		     char cmdstr[MAXSYSCMD];
+		     PKT_SET_INFO_GW_QUICK *gwquick;
+		     PKT_SET_INFO_GW_QUICK *gwquick_res;
+
+		     if (!is_setup_mode(BTNSETUP_DATAEXCHANGE)) return NULL;
+		     gwquick=(PKT_SET_INFO_GW_QUICK *)(pdubuf+sizeof(IBOX_COMM_PKT_HDR_EX));
+		     gwquick_res = (PKT_SET_INFO_GW_QUICK *)(pdubuf_res+sizeof(IBOX_COMM_PKT_RES_EX));
+
+#ifdef ENCRYPTION
+		     if (gwquick->QuickFlag&QFCAP_ENCRYPTION)
+		     {
+			   // DECRYPTION first
+		     }
+		     else return NULL	
+#endif
+
+		     bs_put_setting(gwquick);
+
+		     if (!(gwquick->QuickFlag&QFCAP_WIRELESS)) // get setting
+		     	 bs_get_setting(gwquick_res);
+		     else memcpy(gwquick_res, gwquick, sizeof(PKT_SET_INFO_GW_QUICK));
+
+#ifdef ENCRYPTION
+		     // ENCRYPTION first
+		     gwquick_res->QuickFlag = (QFCAP_ENCRYPTION|QFCAP_WIRELESS);
+#else
+		     gwquick_res->QuickFlag = (QFCAP_WIRELESS);
+#endif	
+		     sendInfo(sockfd, pdubuf_res);
+		     return pdubuf_res;
+		}
+#endif
+		default:
+			return NULL;	
+	}
+    }
     return NULL;
 }
