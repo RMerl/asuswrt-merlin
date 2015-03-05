d5930e765e40ef5434005d740267fb5ef15831f9
joejoejo <goodpets@gmail.com>
2015-03-05 15:08:51 -0500

    Update wlc_extlog_idstr.h

--- release/src-rt-6.x.4708/include/wlc_extlog_idstr.h
+++ release/src-rt-6.x.4708/include/wlc_extlog_idstr.h
@@ -1,7 +1,7 @@
 /*
  * EXTLOG Module log ID to log Format String mapping table
  *
- * Copyright (C) 2013, Broadcom Corporation
+ * Copyright (C) 2014, Broadcom Corporation
  * All Rights Reserved.
  * 
  * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
@@ -9,7 +9,7 @@
  * or duplicated in any form, in whole or in part, without the prior
  * written permission of Broadcom Corporation.
  *
- * $Id: wlc_extlog_idstr.h 419467 2013-08-21 09:19:48Z $
+ * $Id: wlc_extlog_idstr.h 241182 2011-02-17 21:50:03Z $
  */
 #ifndef _WLC_EXTLOG_IDSTR_H_
 #define _WLC_EXTLOG_IDSTR_H_
