1c6374d8beec65c2612f88c6b61f603982bfd0c0
Eric Sauvageau <rmerl@lostrealm.ca>
2016-03-25 18:13:44 -0400

    rc: tmpstr is needed outside of just with built-in app support

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -3084,9 +3084,9 @@ void start_upnp(void)
 	int unit, i, httpx_port;
 #if defined(RTCONFIG_APP_PREINSTALLED) || defined(RTCONFIG_APP_NETINSTALLED) || defined(RTCONFIG_APP_NOLOCALDM)
 	FILE *ifp = NULL;
-	char tmpstr[80];
 	int statDownloadMaster = 0;
 #endif
+	char tmpstr[80];
 	int min_lifetime, max_lifetime;
 
 	if (getpid() != 1) {
