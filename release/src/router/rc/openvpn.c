9993b4bdcced87188917fd82b3abe9b528fc127d
Eric Sauvageau <rmerl@lostrealm.ca>
2016-04-27 02:05:05 -0400

    openvpn: Policy-rule mode is 2, not 3.  Properly skip modifying dnsmasq while in policy mode.

--- release/src/router/rc/openvpn.c
+++ release/src/router/rc/openvpn.c
@@ -1756,7 +1756,7 @@ int write_vpn_resolv(FILE* f)
 
 			// Don't modify dnsmasq if policy routing is enabled and dns mode set to "Exclusive"
 			snprintf(&buf[0], sizeof(buf), "vpn_client%c_rgw", num);
-			if ((nvram_get_int(&buf[0]) == 3 ) && (level == 3))
+			if ((nvram_get_int(&buf[0]) == 2 ) && (level == 3))
 				continue;
 
 			if ( (dnsf = fopen(fn, "r")) == NULL )
