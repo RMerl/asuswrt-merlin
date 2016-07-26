0eed731a51a82923b617cb7b61a2aaaf66bf7a91
Eric Sauvageau <rmerl@lostrealm.ca>
2016-07-26 13:48:18 -0400

    Kludge to support 34xx binary blobs with GPL 3831, for the AC3200 and AC87

--- release/src/router/shared/shared.h
+++ release/src/router/shared/shared.h
@@ -291,6 +291,11 @@ enum {
 	MODEL_PLN12,
 	MODEL_PLAC56,
 	MODEL_PLAC66U,
+#if defined (RTAC3200) || defined(RTAC87U)
+        MODEL_RTAC88N,
+        MODEL_BRTAC828M2,
+        MODEL_RTAC88S,
+#endif
 	MODEL_RTN36U3,
 	MODEL_RTN56U,
 	MODEL_RTN65U,
@@ -328,9 +333,13 @@ enum {
 	MODEL_RTN10PV2,
 	MODEL_RTAC1200G,
 	MODEL_RTAC1200GP,
+#if !defined (RTAC3200) && !defined(RTAC87U)
 	MODEL_RTAC88N,
 	MODEL_BRTAC828M2,
 	MODEL_RTAC88S
+#else
+	MODEL_LAST
+#endif
 };
 
 /* NOTE: Do not insert new entries in the middle of this enum,
