d69b254adf8722c0c0867188c3c76a140e8a74df
Eric Sauvageau <rmerl@lostrealm.ca>
2016-08-02 19:56:13 -0400

    rc: Move OpenVPN nvram state initialization from init_nvram() to init_nvram2() (fixes #990); rewrite part of it as a loop
    init_nvram() is called in a couple of places, including in restart_wireless().  This would lead
    to the OpenVPN state to be lost any time Wireless was restarted.  init_nvram2() is only called
    at boot time, and is the logical place for such a boot-time setup.

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -2185,27 +2185,6 @@ int init_nvram(void)
 #if 0
 	conf_swmode_support(model);
 #endif
-#ifdef RTCONFIG_OPENVPN
-	nvram_set("vpn_server1_state", "0");
-	nvram_set("vpn_server2_state", "0");
-	nvram_set("vpn_client1_state", "0");
-	nvram_set("vpn_client2_state", "0");
-	nvram_set("vpn_client3_state", "0");
-	nvram_set("vpn_client4_state", "0");
-	nvram_set("vpn_client5_state", "0");
-	nvram_set("vpn_server1_errno", "0");
-	nvram_set("vpn_server2_errno", "0");
-	nvram_set("vpn_client1_errno", "0");
-	nvram_set("vpn_client2_errno", "0");
-	nvram_set("vpn_client3_errno", "0");
-	nvram_set("vpn_client4_errno", "0");
-	nvram_set("vpn_client5_errno", "0");
-	nvram_set("vpn_upload_state", "");
-	if(!nvram_is_empty("vpn_server_clientlist")) {
-		nvram_set("vpn_serverx_clientlist", nvram_safe_get("vpn_server_clientlist"));
-		nvram_unset("vpn_server_clientlist");
-	}
-#endif
 
 #ifdef RTCONFIG_DSL_TCLINUX
 	nvram_set("dsllog_opmode", "");
@@ -5560,6 +5539,8 @@ int init_nvram2(void)
 	char *macp = NULL;
 	unsigned char mac_binary[6];
 	char friendly_name[32];
+	int i;
+	char varname[20];
 
 	macp = get_lan_hwaddr();
 	ether_atoe(macp, mac_binary);
@@ -5568,14 +5549,37 @@ int init_nvram2(void)
 #else
 	sprintf(friendly_name, "%s-%02X%02X", get_productid(), mac_binary[4], mac_binary[5]);
 #endif
+
+#ifdef RTCONFIG_OPENVPN
+	for (i = 1; i <= MAX_OVPN_CLIENT; i++) {
+		sprintf(varname, "vpn_client%d_state", i);
+		nvram_set(varname, "0");
+
+		sprintf(varname, "vpn_client%d_errno", i);
+		nvram_set(varname, "0");
+	}
+
+	nvram_set("vpn_server1_state", "0");
+	nvram_set("vpn_server2_state", "0");
+	nvram_set("vpn_server1_errno", "0");
+	nvram_set("vpn_server2_errno", "0");
+	nvram_set("vpn_upload_state", "");
+
+	if(!nvram_is_empty("vpn_server_clientlist")) {
+		nvram_set("vpn_serverx_clientlist", nvram_safe_get("vpn_server_clientlist"));
+		nvram_unset("vpn_server_clientlist");
+	}
+#endif
+
 	if (restore_defaults_g)
 	{
 		nvram_set("computer_name", friendly_name);
 		nvram_set("dms_friendly_name", friendly_name);
 		nvram_set("daapd_friendly_name", friendly_name);
 
-		nvram_commit();
 	}
+
+	nvram_commit();
 
 	return 0;
 }
