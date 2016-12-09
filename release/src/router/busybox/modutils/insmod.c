7ba93145d3193f55e41219f2e6f8afa3eb59462c
Vladislav Grishenko <themiron@mail.ru>
2016-12-10 00:44:01 +0500

    busybox: insmod: allow module names without extenstion and full path

--- release/src/router/busybox/modutils/insmod.c
+++ release/src/router/busybox/modutils/insmod.c
@@ -10,6 +10,7 @@
 //applet:IF_INSMOD(APPLET(insmod, BB_DIR_SBIN, BB_SUID_DROP))
 
 #include "libbb.h"
+#include "common_bufsiz.h"
 #include "modutils.h"
 
 /* 2.6 style insmod has no options and required filename
@@ -35,11 +36,40 @@
 //usage:	)
 //usage:#endif
 
+struct globals {
+	char *filename;
+} FIX_ALIASING;
+#define G (*(struct globals*)bb_common_bufsiz1)
+#define INIT_G() do { \
+	setup_common_bufsiz(); \
+} while (0)
+
+static int FAST_FUNC check_module_name_match(const char *filename,
+		struct stat *statbuf UNUSED_PARAM,
+		void *userdata, int depth UNUSED_PARAM)
+{
+	char *fullname = (char *) userdata;
+	char *tmp;
+
+	if (fullname[0] == '\0')
+		return FALSE;
+
+	tmp = bb_get_last_path_component_nostrip(filename);
+	if (strcmp(tmp, fullname) == 0) {
+		/* Stop searching if we find a match */
+		G.filename = xstrdup(filename);
+		return FALSE;
+	}
+	return TRUE;
+}
+
 int insmod_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
 int insmod_main(int argc UNUSED_PARAM, char **argv)
 {
-	char *filename;
+	char *filename, *dot;
 	int rc;
+
+	INIT_G();
 
 	/* Compat note:
 	 * 2.6 style insmod has no options and required filename
@@ -58,9 +88,36 @@ int insmod_main(int argc UNUSED_PARAM, char **argv)
 	if (!filename)
 		bb_show_usage();
 
+	/* Check if we have a complete module name */
+	dot = strrchr(filename, '.');
+	if (ENABLE_FEATURE_2_4_MODULES
+	 && get_linux_version_code() < KERNEL_VERSION(2,6,0)) {
+		if (!dot || strcmp(dot, ".o") != 0)
+			filename = xasprintf("%s.o", filename);
+	} else {
+		if (!dot || strcmp(dot, ".ko") != 0)
+			filename = xasprintf("%s.ko", filename);
+	}
+
+	/* Check if we have a complete path */
+	if (access(filename, R_OK) != 0) {
+		/* Hmm.  Could not open it. Search /lib/modules/ */
+		char *module_dir = xmalloc_readlink(CONFIG_DEFAULT_MODULES_DIR);
+		if (!module_dir)
+			module_dir = xstrdup(CONFIG_DEFAULT_MODULES_DIR);
+		rc = recursive_action(module_dir, ACTION_RECURSE,
+				check_module_name_match, NULL, filename, 0);
+		if (!rc && G.filename && access(G.filename, R_OK) == 0)
+			filename = G.filename;
+		free(module_dir);
+	}
+
 	rc = bb_init_module(filename, parse_cmdline_module_options(argv, /*quote_spaces:*/ 0));
 	if (rc)
 		bb_error_msg("can't insert '%s': %s", filename, moderror(rc));
+
+	if (ENABLE_FEATURE_CLEAN_UP)
+		free(G.filename);
 
 	return rc;
 }
