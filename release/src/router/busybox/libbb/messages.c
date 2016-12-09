8870d6f19c58d4196d8d03f2500e9174f7295c29
Vladislav Grishenko <themiron@mail.ru>
2016-12-10 03:11:42 +0500

    busybox: add optware/entware support by default

--- release/src/router/busybox/libbb/messages.c
+++ release/src/router/busybox/libbb/messages.c
@@ -40,7 +40,7 @@ const char bb_default_login_shell[] ALIGN1 = LIBBB_DEFAULT_LOGIN_SHELL;
 /* util-linux manpage says /sbin:/bin:/usr/sbin:/usr/bin,
  * but I want to save a few bytes here. Check libbb.h before changing! */
 const char bb_PATH_root_path[] ALIGN1 =
-	"PATH=/sbin:/usr/sbin:/bin:/usr/bin" BB_ADDITIONAL_PATH;
+	"PATH=/sbin:/usr/sbin:/opt/sbin:/bin:/usr/bin:/opt/bin" BB_ADDITIONAL_PATH;
 
 
 //const int const_int_1 = 1;
