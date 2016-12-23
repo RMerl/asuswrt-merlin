5efb1d9514ac690a0f4d05c95ca9ea39bac32013
Eric Sauvageau <rmerl@lostrealm.ca>
2016-12-23 01:47:37 -0500

    openvpn: client config must not contain the client option when using pre-shared key auth

--- release/src/router/rc/openvpn.c
+++ release/src/router/rc/openvpn.c
@@ -782,10 +782,10 @@ void start_vpnserver(int serverNum)
 
 	sprintf(&buffer[0], "/etc/openvpn/server%d/client.ovpn", serverNum);
 	fp_client = fopen(&buffer[0], "w");
-	fprintf(fp_client, "client\n");
 
 	if ( cryptMode == TLS )
 	{
+		fprintf(fp_client, "client\n");
 		if ( ifType == TUN )
 		{
 			fprintf(fp, "topology subnet\n");
