RMerl/asuswrt-merlin

commit daa0df6acc814ac0cf33709cb2618e578d75fe8f

openvpn: Implemented policy routing based on destination IP; Added CIDR notation support to define subnets

release/src/router/others/vpnrouting.sh
@@ -1,53 +1,42 @@
 #!/bin/sh
 
-update_client_list(){
-	# delete rules for IPs not on list
-	OLDIFS=$IFS
-	IP_LIST=`ip rule show|awk '$2 == "from" && $4=="lookup" && $5==ENVIRON["VPN_TBL"] {print $3}'`
-	for IP in $IP_LIST
-	do
-		DEL_IP="y"
-		IFS="<"
-		for ENTRY in $VPN_IP_LIST
-		do
-			VPN_IP=$(echo $ENTRY | cut -d ">" -f 2)
-			if [ "$IP" == "$VPN_IP" ]
-			then
-				DEL_IP=
-			fi
-		done
-		IFS=$OLDIFS
-		if [ "$DEL_IP" == "y" ]
-		then
-			ip rule del from $IP table $VPN_TBL
-			logger -t "openvpn-routing" "Removing $IP from selective routing"
-		fi
-	done
-
-	# add missing rules
+create_client_list(){
 	IFS="<"
 	for ENTRY in $VPN_IP_LIST
 	do
 		VPN_IP=$(echo $ENTRY | cut -d ">" -f 2)
-		if [ "$VPN_IP" != "" ]
+		if [ $VPN_IP != "0.0.0.0" ]
 		then
-			IP_LIST=`ip rule show|awk '$2=="from" && $3==ENVIRON["VPN_IP"] && $4=="lookup" && $5==ENVIRON["VPN_TBL"] {print $3}'`
-			if [ "$IP_LIST" == "" ]
-			then
-				ip rule add from $VPN_IP table $VPN_TBL
-				logger -t "openvpn-routing" "Adding $VPN_IP to selective routing"
-			fi
+			SRCC="from"
+			SRCA="$VPN_IP"
+		else
+			SRCC=""
+			SRCA=""
+		fi
+		DST_IP=$(echo $ENTRY | cut -d ">" -f 3)
+		if [ $DST_IP != "0.0.0.0" ]
+		then
+			DSTC="to"
+			DSTA="$DST_IP"
+		else
+			DSTC=""
+			DSTA=""
+		fi
+		if [ "$SRCC" != "" -o "$DSTC" != "" ]
+		then
+			ip rule add $SRCC $SRCA $DSTC $DSTA table $VPN_TBL
+			logger -t "openvpn-routing" "Added $VPN_IP through $DST_IP to routing policy"
 		fi
 	done
 	IFS=$OLDIFS
 }
 
 purge_client_list(){
-	IP_LIST=`ip rule show|awk '$2 == "from" && $4=="lookup" && $5==ENVIRON["VPN_TBL"] {print $3}'`
-	for IP in $IP_LIST
+	IP_LIST=$(ip rule show|grep "lookup $VPN_TBL" | cut -d ":" -f 1)
+	for PRIO in $IP_LIST
 	do
-		ip rule del from $IP table $VPN_TBL
-		logger -t "openvpn-routing" "Removing $IP from selective routing"
+		ip rule del prio $PRIO
+		logger -t "openvpn-routing" "Removing rule $PRIO from routing policy"
 	done
 }
 
@@ -81,49 +70,48 @@ export VPN_GW VPN_IP VPN_TBL VPN_FORCE
 
 if [ $script_type = "rmupdate" ]
 then
-	logger -t "openvpn-routing" "Refreshing client rules"
-	if [ $VPN_FORCE == "1" && $VPN_REDIR == "2" ]
+	logger -t "openvpn-routing" "Refreshing policy rules"
+	purge_client_list
+	if [ $VPN_FORCE == "1" -a $VPN_REDIR == "2" ]
 	then
 		ip route add unreachable default table $VPN_TBL
-		update_client_list
+		create_client_list
 	else
-		purge_client_list
 		ip route del unreachable default table $VPN_TBL
 	fi
 	ip route flush cache
 	exit 0
 fi
 
-if [ $script_type == "route-up" && $VPN_REDIR != "2" ]
+if [ $script_type == "route-up" -a $VPN_REDIR != "2" ]
 then
-	logger -t "openvpn-routing" "Skipping, not in selective redir mode"
+	logger -t "openvpn-routing" "Skipping, not in routing policy mode"
 	run_custom_script
 	exit 0
 fi
 
-logger -t "openvpn-routing" "Configuring selective routing for $dev"
+logger -t "openvpn-routing" "Configuring policy rules for $dev"
 
 if [ $script_type == "route-pre-down" ]
 then
 
 # Delete tunnel's default route
 	ip route del default table $VPN_TBL
-
-        if [ $VPN_FORCE == "1" ]
-        then
-# Prevent WAN access
-		logger -t "openvpn-routing" "Tunnel down - VPN client access blocked"
-                ip route add unreachable default table $VPN_TBL
-		update_client_list
-        else
 # Delete rules
-		purge_client_list
+	purge_client_list
+
+	if [ $VPN_FORCE == "1" ]
+	then
+# Prevent WAN access
+			logger -t "openvpn-routing" "Tunnel down - VPN client access blocked"
+			ip route add unreachable default table $VPN_TBL
+			create_client_list
 	fi
 fi	# End route down
 
 
 # Delete existing VPN routes (both up and down events)
-NET_LIST=`ip route show|awk '$2=="via" && $3==ENVIRON["route_vpn_gateway"] && $4=="dev" && $5==ENVIRON["dev"] {print $1}'`
+NET_LIST=$(ip route show|awk '$2=="via" && $3==ENVIRON["route_vpn_gateway"] && $4=="dev" && $5==ENVIRON["dev"] {print $1}')
 for NET in $NET_LIST
 do
         ip route del $NET dev $dev
@@ -133,7 +121,8 @@ done
 
 if [ $script_type == "route-up" ]
 then
-	update_client_list
+	purge_client_list
+	create_client_list
 
 # Setup table default route
 	if [ "$VPN_IP_LIST" != "" ]
@@ -154,7 +143,7 @@ then
 fi	# End route-up
 
 ip route flush cache
-logger -t "openvpn-routing" "Completed routing configuration"
+logger -t "openvpn-routing" "Completed routing policy configuration"
 run_custom_script
 
 exit 0

release/src/router/www/Advanced_OpenVPNClient_Content.asp
@@ -485,9 +485,9 @@ function showclientlist(){
 			code +='<tr id="row'+i+'">';
 			var clientlist_col = clientlist_row[i].split('&#62');
 				for(var j = 0; j < clientlist_col.length; j++){
-					code +='<td width="40%">'+ clientlist_col[j] +'</td>';
+					code +='<td width="29%">'+ clientlist_col[j] +'</td>';
 				}
-				code +='<td width="20%">';
+				code +='<td width="13%">';
 				code +='<input class="remove_btn" onclick="del_Row(this);" value=""/></td></tr>';
 		}
 	}
@@ -514,20 +514,28 @@ function addRow_Group(upper){
 		return false;
 	}
 
-	if(document.form.clientlist_ipAddr.value==""){
-		alert("<#JS_fieldblank#>");
-		document.form.clientlist_ipAddr.focus();
-		document.form.clientlist_ipAddr.select();
-		return false;
-	}else if(document.form.clientlist_ipAddr.value==""){
+	if(document.form.clientlist_ipAddr.value=="")
+		document.form.clientlist_ipAddr.value="0.0.0.0";
+
+	if(document.form.clientlist_dstipAddr.value=="")
+		document.form.clientlist_dstipAddr.value="0.0.0.0";
+
+	if (!validate_ipcidr(document.form.clientlist_ipAddr)) {
 		document.form.clientlist_ipAddr.focus();
 		document.form.clientlist_ipAddr.select();
 		return false;
 	}
 
+	if (!validate_ipcidr(document.form.clientlist_dstipAddr)) {
+		document.form.clientlist_dstipAddr.focus();
+		document.form.clientlist_dstipAddr.select();
+		return false;
+	}
+
 	if(item_num >=2){
 		for(i=0; i<rule_num; i++){
-				if(document.form.clientlist_ipAddr.value.toLowerCase() == $('clientlist_table').rows[i].cells[1].innerHTML.toLowerCase()){
+				if(document.form.clientlist_ipAddr.value.toLowerCase() == $('clientlist_table').rows[i].cells[1].innerHTML.toLowerCase() &&
+				   document.form.clientlist_dstipAddr.value.toLowerCase() == $('clientlist_table').rows[i].cells[2].innerHTML.toLowerCase()){
 					alert("<#JS_duplicate#>");
 					document.form.clientlist_ipAddr.focus();
 					document.form.clientlist_ipAddr.select();
@@ -538,6 +546,8 @@ function addRow_Group(upper){
 
 	addRow(document.form.clientlist_deviceName ,1);
 	addRow(document.form.clientlist_ipAddr, 0);
+	addRow(document.form.clientlist_dstipAddr, 0);
+
 	showclientlist();
 }
 
@@ -616,6 +626,20 @@ function pullLANIPList(obj){
 	}
 	else
 		hideClients_Block();
+}
+
+function validate_ipcidr(obj){
+	var rangere_cidr=new RegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$", "gi");
+
+	if(rangere_cidr.test(obj.value) || validator.ipAddr4(obj)) {;
+		return true;
+	}else{
+		alert(obj.value+" <#JS_validip#>");
+		obj.focus();
+		obj.select();
+		return false;
+	}
+
 }
 
 </script>
@@ -1017,8 +1041,8 @@ function pullLANIPList(obj){
 						<td colspan="2">
 							<select name="vpn_client_rgw" class="input_option" onChange="update_visibility();">
 								<option value="0" <% nvram_match("vpn_client_rgw","0","selected"); %>>No</option>
-								<option value="1" <% nvram_match("vpn_client_rgw","1","selected"); %>>All clients</option>
-								<option value="2" <% nvram_match("vpn_client_rgw","2","selected"); %>>Selective clients</option>
+								<option value="1" <% nvram_match("vpn_client_rgw","1","selected"); %>>All traffic</option>
+								<option value="2" <% nvram_match("vpn_client_rgw","2","selected"); %>>Policy rules</option>
 							</select>
 							<label style="padding-left:3em;" id="client_gateway_label">Gateway:</label><input type="text" maxlength="15" class="input_15_table" id="vpn_client_gw" name="vpn_client_gw" onkeypress="return validator.isIPAddr(this, event);" value="<% nvram_get("vpn_client_gw"); %>">
 						</td>
@@ -1035,24 +1059,28 @@ function pullLANIPList(obj){
 				<table id="selectiveTable" width="100%" border="1" align="center" cellpadding="4" cellspacing="0" class="FormTable_table" style="margin-top:8px;">
 					<thead>
 						<tr>
-							<td colspan="3">Clients to route through the tunnel&nbsp;(<#List_limit#>&nbsp;64)</td>
+							<td colspan="4">Rules for routing client traffic through the tunnel (<#List_limit#>&nbsp;64)</td>
 						</tr>
 					</thead>
 					<tr>
-						<th><#ShareNode_DeviceName_itemname#></th>
-						<th><#IPConnection_ExternalIPAddress_itemname#></th>
+						<th><#IPConnection_autofwDesc_itemname#></th>
+						<th>Source IP</th>
+						<th>Destination IP</th>
 						<th><#list_add_delete#></th>
 					</tr>
 					<tr>
-						<td width="40%">
-							<input type="text" class="input_20_table" maxlength="15" name="clientlist_deviceName" onClick="hideClients_Block();" onkeypress="return is_alphanum(this,event);">
+						<td width="29%">
+							<input type="text" class="input_18_table" maxlength="15" name="clientlist_deviceName" onClick="hideClients_Block();" onkeypress="return is_alphanum(this,event);">
+						</td>
+						<td width="29%">
+							<input type="text" class="input_18_table" maxlength="18" name="clientlist_ipAddr" onKeyPressdisab="return validate_ipcidr(this,event)">
 							<img id="pull_arrow" height="14px;" src="/images/arrow-down.gif" style="position:absolute;*margin-left:-3px;*margin-top:1px;" onclick="pullLANIPList(this);" title="<#select_device_name#>" onmouseover="over_var=1;" onmouseout="over_var=0;">
 							<div id="ClientList_Block_PC" class="ClientList_Block_PC"></div>
 						</td>
-						<td width="40%">
-							<input type="text" class="input_20_table" maxlength="15" name="clientlist_ipAddr" onKeyPress="return validator.isIPAddr(this,event)">
+						<td width="29%">
+							<input type="text" class="input_18_table" maxlength="18" name="clientlist_dstipAddr" onKeyPressdisab="return validator.isIPAddr(this,event)">
 						</td>
-						<td width="20%">
+						<td width="13%">
 							<div>
 								<input type="button" class="add_btn" onClick="addRow_Group(64);" value="">
 							</div>

commit 165935747cec35e421b326bf7f42d0fa2bc59bff

vpnrouting: fix IFS not being stored before changing (broken since commit daa0df6acc814ac0cf33709cb2618e578d75fe8f)

release/src/router/others/vpnrouting.sh
@@ -8,6 +8,7 @@ then
 fi
 
 create_client_list(){
+	OLDIFS=$IFS
 	IFS="<"
 
 	for ENTRY in $VPN_IP_LIST