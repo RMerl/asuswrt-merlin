e511da48f7800358c6a32eed001d2fc5aee4888f
Eric Sauvageau <rmerl@lostrealm.ca>
2017-01-20 17:35:27 -0500

    cstats/rstats: update unset clock detection, by replacing the old Y2K value with Jan-1-2017; cast constant as unsigned long.
    At least based on syslog datestamps, at some point during boot the clock gets set to July 2015,
    before being finally set through NTP.

--- release/src/router/rstats/rstats.c
+++ release/src/router/rstats/rstats.c
@@ -53,7 +53,7 @@
 #define SMIN	60
 #define	SHOUR	(60 * 60)
 #define	SDAY	(60 * 60 * 24)
-#define Y2K		1447240271UL
+#define BOOT_DATESTAMP		1483232400UL
 
 #define INTERVAL		30
 #if defined(RTCONFIG_WANPORT2)
@@ -285,7 +285,7 @@ static void save(int quick)
 	comp(speed_fn, speed, sizeof(speed[0]) * speed_count);
 
 /*
-	if ((now = time(0)) < Y2K) {
+	if ((now = time(0)) < BOOT_DATESTAMP) {
 		_dprintf("%s: time not set\n", __FUNCTION__);
 		return;
 	}
@@ -920,7 +920,7 @@ loopagain:
 				c = tmp->counter[i];
 				sc = sp->last[i];
 				if (c < sc) {
-					diff = (0xFFFFFFFF - sc + 1) + c;
+					diff = (0xFFFFFFFFUL - sc + 1) + c;
 					if (diff > MAX_ROLLOVER) diff = 0;
 				}
 				else {
@@ -940,7 +940,7 @@ loopagain:
 
 		// todo: split, delay
 
-		if (now > Y2K && strcmp(tmp->desc, "INTERNET")==0) {
+		if (now > BOOT_DATESTAMP && strcmp(tmp->desc, "INTERNET")==0) {
 			/* Skip this if the time&date is not set yet */
 			/* Skip non-INTERNET interface only 	     */
 			tms = localtime(&now);
