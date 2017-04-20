5f03d86399b150f0267dcb7600d8737d32242545
Eric Sauvageau <rmerl@lostrealm.ca>
2017-04-20 19:53:21 -0400

    httpd: rewrote gencert.sh to insert SANs into the generated SSL certificate
    CNs are being deprecated by browsers.  SANs will be added for DUT,
    hostname (FQDN or not), IP, and DDNS.

--- release/src/router/httpd/gencert.sh
+++ release/src/router/httpd/gencert.sh
@@ -3,37 +3,105 @@ SECS=1262278080
 
 cd /etc
 
-NVCN=`nvram get https_crt_cn`
-if [ "$NVCN" == "" ]; then
-	NVCN=`nvram get lan_ipaddr`
-fi
-
 cp -L openssl.cnf openssl.config
 
+LANCN=$(nvram get https_crt_cn)
+LANIP=$(nvram get lan_ipaddr)
+
+if [ "$LANCN" != "" ]
+then
+	I=0
+	for CN in $LANCN; do
+		echo "$I.commonName=CN" >> openssl.config
+		echo "$I.commonName_value=$CN" >> openssl.config
+		I=$(($I + 1))
+	done
+else
+	echo "0.commonName=CN" >> openssl.config
+	echo "0.commonName_value=$LANIP" >> openssl.config
+fi
+
 I=0
-for CN in $NVCN; do
-        echo "$I.commonName=CN" >> openssl.config
-        echo "$I.commonName_value=$CN" >> openssl.config
-        I=$(($I + 1))
-done
+# Start of SAN extensions
+sed -i "/\[ CA_default \]/acopy_extensions = copy" openssl.config
+sed -i "/\[ v3_ca \]/asubjectAltName = @alt_names" openssl.config
+sed -i "/\[ v3_req \]/asubjectAltName = @alt_names" openssl.config
+echo "[alt_names]" >> openssl.config
 
-# create the key and certificate request
-#openssl req -new -out /tmp/cert.csr -config openssl.config -keyout /tmp/privkey.pem -newkey rsa:1024 -passout pass:password
-# remove the passphrase from the key
-#openssl rsa -in /tmp/privkey.pem -out key.pem -passin pass:password
-# convert the certificate request into a signed certificate
-#openssl x509 -in /tmp/cert.csr -out cert.pem -req -signkey key.pem -setstartsecs $SECS -days 3653 -set_serial $1
+# IP
+echo "IP.0 = $LANIP" >> openssl.config
 
-# create the key and certificate request
-OPENSSL_CONF=/etc/openssl.config openssl req -new -out /tmp/cert.csr -keyout /tmp/privkey.pem -newkey rsa:2048 -passout pass:password
-# remove the passphrase from the key
-OPENSSL_CONF=/etc/openssl.cnf openssl rsa -in /tmp/privkey.pem -out key.pem -passin pass:password
-# convert the certificate request into a signed certificate
-OPENSSL_CONF=/etc/openssl.cnf RANDFILE=/dev/urandom openssl x509 -in /tmp/cert.csr -out cert.pem -req -signkey key.pem -days 3653 -sha256
+# DUT
+echo "DNS.$I = router.asus.com" >> openssl.config
+I=$(($I + 1))
+
+# User-defined CN (if we have any - legacy Tomato code?)
+if [ "$NVCN" != "" ]
+then
+	for CN in $NVCN; do
+		echo "DNS.$I = $CN" >> openssl.config
+		I=$(($I + 1))
+	done
+fi
+
+# hostnames
+
+LANDOMAIN=$(nvram get lan_domain)
+COMPUTERNAME=$(nvram get computer_name)
+LANHOSTNAME=$(nvram get lan_hostname)
+
+if [ "$COMPUTERNAME" != "" ]
+then
+	echo "DNS.$I = $COMPUTERNAME" >> openssl.config
+	I=$(($I + 1))
+
+	if [ "$LANDOMAIN" != "" ]
+	then
+		echo "DNS.$I = $COMPUTERNAME.$LANDOMAIN" >> openssl.config
+		I=$(($I + 1))
+	fi
+fi
+
+if [ "$LANHOSTNAME" != "" ]
+then
+	echo "DNS.$I = $LANHOSTNAME" >> openssl.config
+	I=$(($I + 1))
+
+	if [ "$LANDOMAIN" != "" ]
+	then
+		echo "DNS.$I = $LANHOSTNAME.$LANDOMAIN" >> openssl.config
+		I=$(($I + 1))
+	fi
+fi
+
+
+# DDNS
+DDNSHOSTNAME=$(nvram get ddns_hostname_x)
+DDNSSERVER=$(nvram get ddns_server_x)
+DDNSUSER=$(nvram get ddns_username_x)
+
+if [ "$(nvram get ddns_enable_x)" == "1" -a "$DDNSSERVER" != "WWW.DNSOMATIC.COM" -a "$DDNSHOSTNAME" != "" ]
+then
+	if [ "$DDNSSERVER" == "WWW.NAMECHEAP.COM" -a "$DDNSUSER" != "" ]
+	then
+		echo "DNS.$I = $DDNSHOSTNAME.$DDNSUSER" >> openssl.config
+		I=$(($I + 1))
+	else
+		echo "DNS.$I = $DDNSHOSTNAME" >> openssl.config
+		I=$(($I + 1))
+	fi
+fi
+
+
+# create the key
+openssl genrsa -out key.pem 2048 -config /etc/openssl.config
+# create certificate request and sign it
+RANDFILE=/dev/urandom openssl req -new -x509 -key key.pem -sha256 -out cert.pem -days 3653 -config /etc/openssl.config
+
 
 #	openssl x509 -in /etc/cert.pem -text -noout
 
 # server.pem for WebDav SSL
 cat key.pem cert.pem > server.pem
 
-rm -f /tmp/cert.csr /tmp/privkey.pem openssl.config
+rm -f /tmp/cert.csr /etc/openssl.config
