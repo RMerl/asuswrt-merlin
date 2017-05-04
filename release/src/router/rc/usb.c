4fb815d2d9009f5ebf09d1914041b0fde6ef526b
Eric Sauvageau <rmerl@lostrealm.ca>
2017-05-04 00:33:22 -0400

    minidlna: fix previous commit (missing function, typo, and also point out this is for minidlna, not miniupnpd)"

--- release/src/router/rc/usb.c
+++ release/src/router/rc/usb.c
@@ -2522,11 +2522,10 @@ void start_dms(void)
 
 			nvram_set("dms_dbcwd", dbdir);
 
-			strlcpy(serial, get_lan_hwaddr(), sizeof (serial));
+			conv_mac2(get_lan_hwaddr(), serial);
 			if (strlen(serial)) {
 				for (i = 0; i < strlen(serial); i++)
 					serial[i] = tolower(serial[i]);
-				remove_char(serial, ':'); //remove colons from mac for serial
 			}
 			else
 				strcpy(serial, "554e4b4e4f57"); //default if no hwaddr
@@ -2606,7 +2605,7 @@ void start_dms(void)
 
 			fprintf(f,
 				"serial=%s\n"
-				"model_number=%s.%s\n",
+				"model_number=%s.%s\n"
 				//add explicit uuid based on mac(serial)
 				//since some recent change has resulted in a changing uuid at boot
 				"uuid=4d696e69-444c-164e-9d41-%s\n",
