c6e7dc2839dcc5a4391e0c50a326187ed950006d
Eric Sauvageau <rmerl@lostrealm.ca>
2017-05-31 14:04:38 -0400

    webui: fix JS error in help_content

--- release/src/router/www/help_content.js
+++ release/src/router/www/help_content.js
@@ -340,6 +340,6 @@ helpcontent[50] = new Array("",
 				"Wanduck sends DNS queries every few seconds to check for the WAN state.  You can disable this behaviour here.",
 				"You can redirect your Internet traffic through the tunnel.  Select between \"All\" traffic, or only traffic based on your own rules.  Strict mode will take extra steps to ensure that only routes going through the VPN tunnel get used for your tunnelled clients.",
 				"Bridge multicast snooping is no longer enabled by default, as it can possibly interfere with various things like IPv6, EMF and UPnP.  Re-enable it if you are encountering performance or stability issues with your LAN.",
-				"Save the SSL certificate so it will be reused every time the web server is restarted.  You can also replace it with your own, stored under /jffs/ssl.");
+				"Save the SSL certificate so it will be reused every time the web server is restarted.  You can also replace it with your own, stored under /jffs/ssl.",
 				"You can specify other hostnames to use in addition to the built-in ones, separated by a space");
 // Last: 50,22
