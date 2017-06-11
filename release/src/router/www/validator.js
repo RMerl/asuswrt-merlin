2701fe778f0a32a3c6590d2eb444c82381c013e2
Eric Sauvageau <rmerl@lostrealm.ca>
2017-06-11 02:39:51 -0400

    webui: make validator.domainName() method allow both hostnames and dotted FQDN (backport from 382)

--- release/src/router/www/validator.js
+++ release/src/router/www/validator.js
@@ -318,7 +318,7 @@ var validator = {
 	},
 
 	domainName: function (obj) { //support a-z, 0-9, "-", "_" , "."", The first character cannot be dash "-" or under line "_"
-		var re = new RegExp(/^(?:[a-z0-9](?:[a-z0-9-_]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]$/);
+		var re = new RegExp(/^(?:[a-z0-9](?:[a-z0-9-_]{0,61}[a-z0-9])?\.)*[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]$/);
 		if(re.test(obj.value)){
 			return "";
 		}
