bd426cb44dac760039890a52e4c02efb08edbf03
Vladislav Grishenko <themiron@mail.ru>
2017-11-01 21:38:53 +0500

    odhcp6c: md5: size-optimize the code, performance doesn't really matter
    $ bloat-o-meter odhcp6c.orig odhcp6c
    function                                             old     new   delta
    .rodata                                             2496    2832    +336
    static.C_array                                         -     256    +256
    static.P_array                                         -      64     +64
    static.S_array                                         -      16     +16
    body                                                6212     768   -5444
    ------------------------------------------------------------------------------
    (add/remove: 3/0 grow/shrink: 1/1 up/down: 672/-5444)       Total: -4772 bytes

--- release/src/router/odhcp6c/src/md5.c
+++ release/src/router/odhcp6c/src/md5.c
@@ -56,6 +56,8 @@
 
 #include "md5.h"
 
+#define MD5_SMALL
+
 /*
  * The basic MD5 functions.
  *
@@ -72,9 +74,12 @@
 /*
  * The MD5 transformation for all four rounds.
  */
+#define ROTL(a, s) \
+	(((a) << (s)) | (((a) & 0xffffffff) >> (32 - (s))))
+
 #define STEP(f, a, b, c, d, x, t, s) \
 	(a) += f((b), (c), (d)) + (x) + (t); \
-	(a) = (((a) << (s)) | (((a) & 0xffffffff) >> (32 - (s)))); \
+	(a) = ROTL((a), (s)); \
 	(a) += (b);
 
 /*
@@ -103,6 +108,46 @@
  */
 static const void *body(md5_ctx_t *ctx, const void *data, unsigned long size)
 {
+#ifdef MD5_SMALL
+	static const uint32_t C_array[] = {
+		/* round 1 */
+		0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+		0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+		0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+		0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+		/* round 2 */
+		0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+		0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+		0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+		0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+		/* round 3 */
+		0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+		0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+		0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x4881d05,
+		0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+		/* round 4 */
+		0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+		0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+		0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+		0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+	};
+	static const char P_array[] __attribute__((aligned(1))) = {
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,	/* 1 */
+		1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,	/* 2 */
+		5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,	/* 3 */
+		0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9	/* 4 */
+	};
+	static const char S_array[] __attribute__((aligned(1))) = {
+		7, 12, 17, 22,
+		5, 9, 14, 20,
+		4, 11, 16, 23,
+		6, 10, 15, 21
+	};
+	const uint32_t *pc;
+	const char *pp, *ps;
+	uint32_t temp;
+	int i;
+#endif
 	const unsigned char *ptr;
 	uint32_t a, b, c, d;
 	uint32_t saved_a, saved_b, saved_c, saved_d;
@@ -123,6 +168,41 @@ static const void *body(md5_ctx_t *ctx, const void *data, unsigned long size)
 		saved_c = c;
 		saved_d = d;
 
+#ifdef MD5_SMALL
+		pc = C_array;
+		pp = P_array;
+		ps = S_array - 4;
+
+		for (i = 0; i < 64; i++, pp++) {
+			if ((i & 0x0f) == 0)
+				ps += 4;
+
+			temp = a;
+			switch (i >> 4) {
+			case 0:
+				temp += F(b, c, d) + SET((int)*pp);
+				break;
+			case 1:
+				temp += G(b, c, d);
+				goto next;
+			case 2:
+				temp += H(b, c, d);
+				goto next;
+			default:
+				temp += I(b, c, d);
+			next:
+				temp += GET((int)*pp);
+				break;
+			}
+			temp += *pc++;
+			temp = ROTL(temp, ps[i & 3]);
+
+			a = d;
+			d = c;
+			c = b;
+			b += temp;
+		}
+#else
 /* Round 1 */
 		STEP(F, a, b, c, d, SET(0), 0xd76aa478, 7)
 		STEP(F, d, a, b, c, SET(1), 0xe8c7b756, 12)
@@ -194,6 +274,7 @@ static const void *body(md5_ctx_t *ctx, const void *data, unsigned long size)
 		STEP(I, d, a, b, c, GET(11), 0xbd3af235, 10)
 		STEP(I, c, d, a, b, GET(2), 0x2ad7d2bb, 15)
 		STEP(I, b, c, d, a, GET(9), 0xeb86d391, 21)
+#endif
 
 		a += saved_a;
 		b += saved_b;
